73c6bdc8ce64f590d01fe6af8c64fdce8637d990
Wiktor Sacharczuk <44320437+wiktorsachar@users.noreply.github.com>
2021-04-15 16:52:49 +0200

    Add files via upload

---
+++ precache-manifest.8d2bf6a234ef7e8a10dcd9f9b7951e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa2de7075099ecdf114f66224daf7fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53601062003819409e06",
+    "url": "./static/css/main.c569e713.chunk.css"
+  },
+  {
+    "revision": "90978710a5869e3aec1c",
+    "url": "./static/js/2.229f900b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.229f900b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53601062003819409e06",
+    "url": "./static/js/main.191cf41a.chunk.js"
+  },
+  {
+    "revision": "a4ba609ccaf9ba1300a4",
+    "url": "./static/js/runtime-main.79a64ed6.js"
+  },
+  {
+    "revision": "e1eb63edee5cc93db7a03943b799d8fd",
+    "url": "./static/media/TREBUCBD.e1eb63ed.ttf"
+  }
+]);
